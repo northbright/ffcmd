13c23c31a8dcbff27ee35548dea88e8e566d226c
Frank Xu <frank.xu@northbright.com>
2025-06-21 13:07:47 +0800

    Make GenCommand() short

--- cmd.go
+++ cmd.go
@@ -14,20 +14,12 @@ type Cmd interface {
 
 // GenCommand returns an exec.Cmd with given command string.
 func GenCommand(cmdStr string) (*exec.Cmd, error) {
-	sh := ""
-	arg1 := ""
-
 	switch runtime.GOOS {
 	case "darwin":
-		sh = "bash"
-		arg1 = "-c"
+		return exec.Command("bash", "-c", cmdStr), nil
 	case "linux":
-		sh = "bash"
-		arg1 = "-c"
+		return exec.Command("bash", "-c", cmdStr), nil
 	default:
 		return nil, fmt.Errorf("not supported OS")
 	}
-
-	cmd := exec.Command(sh, arg1, cmdStr)
-	return cmd, nil
 }
