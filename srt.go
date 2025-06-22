455ce5f0686ba3b70ee0d1fc705d30b50b4b78ac
Frank Xu <frank.xu@northbright.com>
2025-06-22 13:53:57 +0800

    Use "bc" to do floating math to get end timestamp of a clip

--- srt.go
+++ srt.go
@@ -85,7 +85,7 @@ func (cmd *CreateOneSubSRTCmd) String() (string, error) {
 			return "", fmt.Errorf("both end time and video filename are empty, can not get end timestamp")
 		}
 
-		str = fmt.Sprintf(`sec=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "%s" | awk -F. '{ print $1 }'); sec=$(( sec - %d)); hh=$((sec / 3600)); mm=$((sec %% 3600 / 60)); ss=$((sec %% 3600 %% 60)); printf -v end "%%02d:%%02d:%%02d,000" $hh $mm $ss; echo -ne "1\n00:00:00,000 --> $end\n%s" > "%s"`, cmd.videoFile, int(startSec), cmd.text, cmd.srtFile)
+		str = fmt.Sprintf(`sec=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "%s"); sec=$(echo $sec - %.03f | bc); sec=$(echo $sec | awk -F. '{ print $1 }'); hh=$((sec / 3600)); mm=$((sec %% 3600 / 60)); ss=$((sec %% 3600 %% 60)); printf -v end "%%02d:%%02d:%%02d,000" $hh $mm $ss; echo -ne "1\n00:00:00,000 --> $end\n%s" > "%s"`, cmd.videoFile, startSec, cmd.text, cmd.srtFile)
 	} else {
 		if tsEnd, err = NewTimestamp(cmd.end); err != nil {
 			return "", fmt.Errorf("invalid end time format")
